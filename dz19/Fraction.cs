e1b1cc700fe8e9dbf4ca5f390420d92bb7e19758
Maxym_Tomus <101803422+mrmarkiz@users.noreply.github.com>
2023-06-17 17:51:49 +0300

    added task1

---
+++ dz19/Fraction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz19
+{
+    [DataContract]
+
+    internal class Fraction
+    {
+        private static string _path = "fractions.dat";
+
+        [DataMember]
+        public int Numerator { get; set; }
+
+        [DataMember]
+        public int Denominator { get; set; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public Fraction() : this(1, 1)
+        { }
+
+        public static void Save(List<Fraction> fractions)
+        {
+            using (FileStream fs = new FileStream(_path, FileMode.OpenOrCreate))
+            {
+                DataContractSerializer dcs = new DataContractSerializer(typeof(List<Fraction>));
+                dcs.WriteObject(fs, fractions);
+            }
+        }
+
+        public static List<Fraction> Read()
+        {
+            List<Fraction> fracts = new List<Fraction>();
+            using (FileStream fs = new FileStream(_path, FileMode.OpenOrCreate))
+            {
+                DataContractSerializer dcs = new DataContractSerializer(typeof(List<Fraction>));
+                try
+                {
+                    fracts = (List<Fraction>)dcs.ReadObject(fs);
+                }
+                catch
+                {
+                    fracts=new List<Fraction>();
+                }
+            }
+            return fracts;
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+    }
+}
---
+++ dz19/Task1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz19
+{
+    internal class Task1
+    {
+        public static void Run()
+        {
+            List<Fraction> fractions = new List<Fraction>();
+            int choice;
+            do
+            {
+                Console.WriteLine("Enter what to do(1-show current list, 2-add fraction, 3-save current list, 4-read list):");
+                int.TryParse(Console.ReadLine(), out choice);
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Current fractions:");
+                        Console.WriteLine(string.Join("\n", fractions));
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter fraction(use /)");
+                        string[] input = Console.ReadLine().Split('/',' ');
+                        while (input.Length < 2)
+                        {
+                            input = input.Append("1").ToArray();
+                        }
+                        int.TryParse(input[0], out int num);
+                        int.TryParse(input[1], out int denom);
+                        fractions.Add(new Fraction(num, denom));
+                        break;
+
+                    case 3:
+                        Fraction.Save(fractions);
+                        break;
+                    case 4:
+                        fractions = Fraction.Read();
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
